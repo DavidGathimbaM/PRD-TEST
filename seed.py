f3164bd46e1fc60d2d52c837f813c58530188ba1
David Gathimba <davidmgathimba.com>
2026-06-28 14:44:31 +0300

    changes to file structure

--- seed.py
+++
@@ -1,6 +0,0 @@
-from database import init_db, seed_database_if_empty
-
-if __name__ == "__main__":
-    init_db()
-    seed_database_if_empty()
-    print("Database initialized and seeded successfully.")
